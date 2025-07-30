43f4702a129201c36b40fe2a9b481e4c14308955
Amr Khaled <redwolf4325@gmail.com>
2025-07-31 00:56:15 +0300

    Finished Prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import ShopItem from './components/shop-item'
 import Header from './layout/header'
 import Hero from './layout/hero'
 
@@ -7,6 +8,15 @@ function App() {
     <>
       <Header />
       <Hero />
+      <ShopItem item={{
+        title: "Title",
+        img: '/assets/images/card-1.jpg',
+        baths: 2,
+        beds: 3,
+        size: 1200,
+        price: 5000,
+        location: "Austin, Texas"
+      }} />
     </>
   )
 }
---
+++ src/components/shop-item.tsx
@@ -0,0 +1,55 @@
+import { Bath, Bed, MapPin, MoveDiagonal2 } from "lucide-react";
+import { Button } from "./ui/button";
+import { Separator } from "./ui/separator";
+import './style/shop-item.css'
+
+export interface ShopItemType {
+  title: string
+  img: string
+  location: string
+  size: number
+  beds: number
+  baths: number
+  price: number
+}
+
+type Props = {
+  item: ShopItemType
+}
+
+export default function ShopItem({ item }: Props) {
+  return <section className='shop-item-card'>
+    <main className='item-main'>
+      <div className='item-image'>
+        <img src={item.img} alt={item.title} />
+      </div>
+      <h3 className='item-title'>
+        {item.title}
+      </h3>
+      <span className='item-location'>
+        <MapPin />
+        {item.location}
+      </span>
+      <span className='item-details'>
+        <span className='item-info'>
+          <MoveDiagonal2 />
+          {item.size} sq. ft.
+        </span>
+        <Separator orientation="vertical" decorative />
+        <span className='item-info'>
+          <Bed />
+          {item.beds} Bed
+        </span>
+        <Separator orientation="vertical" decorative />
+        <span className='item-info'>
+          <Bath />
+          {item.baths} Bath
+        </span>
+      </span>
+    </main>
+    <footer className='item-footer'>
+      <span className='item-price'>${item.price}</span>
+      <Button size='sm' variant={'outline'}>Invest now</Button>
+    </footer>
+  </section>
+}
