0ced4ca7fd4f8a897ca973f077f49bf7a5e851d7
Amr Khaled <redwolf4325@gmail.com>
2025-07-31 18:04:10 +0300

    Added pagination & fixed the search header

--- src/layout/search.tsx
+++ src/layout/search.tsx
@@ -2,11 +2,119 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { ChevronsUpDownIcon } from "lucide-react"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import './style/search.css'
+import type { ShopItemType } from "@/components/shop-item"
+import ShopItem from "@/components/shop-item"
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination"
 
+let items: ShopItemType[] = [
+  {
+    title: "Card 1",
+    img: '/assets/images/card-1.jpg',
+    location: "Texas",
+    price: 5000,
+    beds: 2,
+    baths: 2,
+    size: 1200
+  },
+  {
+    title: "Card 2",
+    img: '/assets/images/card-2.jpg',
+    location: "Texas",
+    price: 5000,
+    beds: 2,
+    baths: 2,
+    size: 1200
+  },
+  {
+    title: "Card 3",
+    img: '/assets/images/card-3.jpg',
+    location: "Texas",
+    price: 5000,
+    beds: 2,
+    baths: 2,
+    size: 1200
+  },
+  {
+    title: "Card 4",
+    img: '/assets/images/card-4.jpg',
+    location: "Texas",
+    price: 5000,
+    beds: 2,
+    baths: 2,
+    size: 1200
+  },
+  {
+    title: "Card 5",
+    img: '/assets/images/card-5.jpg',
+    location: "Texas",
+    price: 5000,
+    beds: 2,
+    baths: 2,
+    size: 1200
+  },
+  {
+    title: "Card 6",
+    img: '/assets/images/card-6.jpg',
+    location: "Texas",
+    price: 5000,
+    beds: 2,
+    baths: 2,
+    size: 1200
+  },
+  {
+    title: "Card 7",
+    img: '/assets/images/card-7.jpg',
+    location: "Texas",
+    price: 5000,
+    beds: 2,
+    baths: 2,
+    size: 1200
+  },
+  {
+    title: "Card 8",
+    img: '/assets/images/card-8.jpg',
+    location: "Texas",
+    price: 5000,
+    beds: 2,
+    baths: 2,
+    size: 1200
+  },
+]
+const max = items.length * 4
+
+if (items.length < max) {
+  for (let i = 0; i < (max / items.length); i++) {
+    items = items.concat(...items)
+  }
+}
 
 export default function Search() {
+  const [shopItems] = useState<ShopItemType[]>(items)
+  const [paginationIndex, setPaginationIndex] = useState(1)
+  const [maxPaginationIndex, setMaxPaginationIndex] = useState(12)
+  useEffect(() => {
+    setMaxPaginationIndex(Math.ceil(shopItems.length / 9))
+  }, [shopItems.length])
+
+  const prevPagination = () => {
+    if (paginationIndex !== 1) {
+      setPaginationIndex(e => e - 1)
+    }
+  }
+  const nextPagination = () => {
+    if (paginationIndex !== maxPaginationIndex) {
+      setPaginationIndex(e => e + 1)
+    }
+  }
+  const selectPagination = (num: number) => {
+    console.log(num)
+    if (num <= maxPaginationIndex
+      && num >= 1)
+      setPaginationIndex(num)
+  }
+
 
   const [uiSelectOptions] = useState([
     'Flat',
@@ -53,7 +161,13 @@ export default function Search() {
     <main className='search-main'>
       <div className='search-bar'>
         <span className='search-status'>
-          Showing <span className='search-results'>1-9</span> of total { } results
+          Showing <span className='search-results'>
+            {(paginationIndex - 1) * 9}-{
+              paginationIndex * 9 > maxPaginationIndex * 8
+                ? maxPaginationIndex * 8
+                : paginationIndex * 9
+            }
+          </span> of total {maxPaginationIndex * 8} results
         </span>
         <Select onValueChange={e => setSearchFilter(e)}>
           <SelectTrigger>
@@ -66,6 +180,59 @@ export default function Search() {
           </SelectContent>
         </Select>
       </div>
+      <section className='search-items'>
+        {shopItems.slice(
+          (paginationIndex - 1) * 9,
+          paginationIndex * 9 > maxPaginationIndex * 8
+            ? maxPaginationIndex * 8
+            : paginationIndex * 9).map((e, i) => <ShopItem key={i} item={e} />)}
+      </section>
+      <section className='search-pagination'>
+        <Pagination className=''>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious onClick={prevPagination} />
+            </PaginationItem>
+            {paginationIndex !== 1 &&
+              <PaginationItem>
+                <PaginationLink
+                  onClick={() =>
+                    selectPagination(paginationIndex - 1)}>
+                  {paginationIndex - 1}
+                </PaginationLink>
+              </PaginationItem>}
+            <PaginationItem>
+              <PaginationLink isActive>
+                {paginationIndex}
+              </PaginationLink>
+            </PaginationItem>
+            {paginationIndex !== maxPaginationIndex &&
+              <PaginationItem>
+                <PaginationLink
+                  onClick={() =>
+                    selectPagination(paginationIndex + 1)}>
+                  {paginationIndex + 1}
+                </PaginationLink>
+              </PaginationItem>}
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem>
+              {paginationIndex + 5 < maxPaginationIndex &&
+                <PaginationItem>
+                  <PaginationLink
+                    onClick={() =>
+                      selectPagination(paginationIndex + 5)}>
+                    {paginationIndex + 5}
+                  </PaginationLink>
+                </PaginationItem>}
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext onClick={nextPagination} />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </section>
     </main>
   </section>
 }
