2b3dda551fa1caf6de95ced06705366da6c34c7d
Amr Khaled <redwolf4325@gmail.com>
2025-08-05 03:46:35 +0300

    added dropdown menu to make header responsive for mobiles

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,6 +1,9 @@
 import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from '@/components/ui/navigation-menu'
 import './style/header.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem } from '@/components/ui/dropdown-menu'
+import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu'
+import { Sling as Menu } from 'hamburger-react'
 
 export default function Header() {
   const [items] = useState([
@@ -15,23 +18,53 @@ export default function Header() {
     login: '#',
     joinus: '#'
   })
+  const [currentWidth, setWidth] = useState(420)
+  useEffect(() => {
+    const resizeEvent = () => {
+      setWidth(window.innerWidth)
+    }
+    resizeEvent()
+    window.addEventListener('resize', resizeEvent)
+    return () => {
+      window.removeEventListener('resize', resizeEvent)
+    }
+  }, [])
+  const [dropdownOpen, setOpen] = useState(false)
   return <header className='header'>
     <div className='navbar'>
       <span className='logo'>Livlast</span>
-      <NavigationMenu>
-        <NavigationMenuList>
-          {items.map((e, i) => <NavigationMenuItem key={i}>
-            <NavigationMenuLink asChild>
-              <a
-                href={hrefs[
-                  e.toLowerCase().split(' ').join('')
-                ]}
-                className={`nav-link ${items.length - 1 === i ? 'join' : ''}`}>{e}</a>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          )}
-        </NavigationMenuList>
-      </NavigationMenu>
+      {
+        currentWidth > 420
+          ? <NavigationMenu>
+            <NavigationMenuList>
+              {items.map((e, i) => <NavigationMenuItem key={i}>
+                <NavigationMenuLink asChild>
+                  <a
+                    href={hrefs[
+                      e.toLowerCase().split(' ').join('')
+                    ]}
+                    className={`nav-link ${items.length - 1 === i ? 'join' : ''}`}>{e}</a>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              )}
+            </NavigationMenuList>
+          </NavigationMenu>
+          : <DropdownMenu onOpenChange={v => setOpen(v)} defaultOpen={dropdownOpen}>
+            <DropdownMenuTrigger className='focus-visible:outline-none'>
+              <Menu direction="right" size={28} label='Show header dropdown menu' hideOutline={false} toggled={dropdownOpen} />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              {items.map((e, i) => <DropdownMenuItem>
+                <a
+                  href={hrefs[
+                    e.toLowerCase().split(' ').join('')
+                  ]}
+                  className={`nav-link ${items.length - 1 === i ? 'join' : ''}`}>{e}</a>
+              </DropdownMenuItem>)}
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+      }
     </div>
   </header>
 }
